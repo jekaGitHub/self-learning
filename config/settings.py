fea0f006e212f5393c061711c4cf3d9d9a98273a
Evgeny Safonov <jeka-luda@yandex.ru>
2024-06-20 23:04:09 +0300

    create app materials and checking

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users',
+    'materials',
+    'checking',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
+    path('', include('materials.urls', namespace='materials')),
+    path('checking/', include('checking.urls', namespace='checking')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

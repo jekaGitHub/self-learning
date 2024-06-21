c23220202a8a494eb0286409d06cd1ad0f9d2582
Evgeny Safonov <jeka-luda@yandex.ru>
2024-06-21 15:08:28 +0300

    add register in admin

--- checking/admin.py
+++ checking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from checking.models import Question
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "material", "question", "answer_right", "answer_wrong_1", "answer_wrong_2", "answer_wrong_3",)
+    list_filter = ("material",)
+    search_fields = ("question",)
--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Category, Material
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description",)
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "category", "contents", "image",)
+    list_filter = ("category",)
+    search_fields = ("title",)

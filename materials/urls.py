116f5fb3296baa899a56324e1e873268049d8918
Evgeny Safonov <jeka-luda@yandex.ru>
2024-06-22 12:53:15 +0300

    add view and url for index.html

--- materials/urls.py
+++ materials/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from materials.apps import MaterialsConfig
-
+from materials.views import HomeListView
 
 app_name = MaterialsConfig.name
 
 urlpatterns = [
-
+    path('', HomeListView.as_view(), name='home'),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from materials.models import Category, Material
+from django.views.generic import View, CreateView, ListView, DetailView, UpdateView, DeleteView, TemplateView
+
+
+class HomeListView(ListView):
+    model = Category
+    template_name = 'materials/index.html'
+    context_object_name = 'object_list'
+
+    def get_queryset(self):
+        return Category.objects.all()

47493a9121ca0391f430115b33d9bab599ef7a9d
Evgeny Safonov <jeka-luda@yandex.ru>
2024-06-24 23:46:24 +0300

    add QuestionsView

--- checking/views.py
+++ checking/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from materials.models import Material
+from checking.models import Question
+from django.views.generic import View
+
+
+class QuestionsView(View):
+    def get(self, request, pk, *args, **kwargs):
+        material = Material.objects.get(pk=pk)
+        questions = Question.objects.filter(material=material)
+        context = {
+            "questions": questions,
+        }
+        return render(request, 'checking/questions.html', context)

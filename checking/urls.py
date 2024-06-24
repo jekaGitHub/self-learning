abbaf9f82f324460c4e70dc5bf5d3b7b018b38ef
Evgeny Safonov <jeka-luda@yandex.ru>
2024-06-24 23:46:49 +0300

    add url for QuestionsView

--- checking/urls.py
+++ checking/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from checking.apps import CheckingConfig
+from checking.views import QuestionsView
 
 app_name = CheckingConfig.name
 
 urlpatterns = [
-
+    path('<int:pk>/questions', QuestionsView.as_view(), name='questions'),
 ]

9685ad792111ad0bcfe2db45c66c6613ffddcf7a
Evgeny Safonov <jeka-luda@yandex.ru>
2024-06-25 17:07:53 +0300

    add method post for QuestionsView

--- checking/views.py
+++ checking/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from materials.models import Material
-from checking.models import Question
+from checking.models import Question, Answer
 from django.views.generic import View
 
 
@@ -9,7 +9,20 @@ class QuestionsView(View):
     def get(self, request, pk, *args, **kwargs):
         material = Material.objects.get(pk=pk)
         questions = Question.objects.filter(material=material)
+        for question in questions:
+            question.answers_list = Answer.objects.filter(owner=self.request.user, question=question)
         context = {
             "questions": questions,
+            "material": material,
         }
         return render(request, 'checking/questions.html', context)
+
+    def post(self, *args, **kwargs):
+        question_id = self.request.POST.get('id')
+        question = Question.objects.get(id=question_id)
+        answer = self.request.POST.get('answer')
+        if answer == question.answer_right:
+            Answer.objects.create(owner=self.request.user, answer=answer, question=question, is_right=True)
+        else:
+            Answer.objects.create(owner=self.request.user, answer=answer, question=question, is_right=False)
+        return redirect(self.request.META['HTTP_REFERER'])

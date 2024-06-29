fe9e28316f36271e2a5725d79b9c5a2da4228a91
Evgeny Safonov <jeka-luda@yandex.ru>
2024-06-29 12:44:47 +0300

    add output AnswerAdmin

--- checking/admin.py
+++ checking/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from checking.models import Question
+from checking.models import Question, Answer
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("id", "material", "question", "answer_right", "answer_wrong_1", "answer_wrong_2", "answer_wrong_3",)
     list_filter = ("material",)
+    search_fields = ("question",)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "question", "answer", "is_right",)
+    list_filter = ("owner", "is_right",)
     search_fields = ("question",)
